600ce399dba24b4bb7986e24e1305a93ececcabb
hondaaja456 <honda456yuni77@gmail.com>
2026-01-25 15:38:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,4 @@
-<?php
-$posts = [
-  [
-    "title" => "Understanding HTML",
-    "content" => "HTML gives structure to your page."
-  ],
-  [
-    "title" => "Learning CSS",
-    "content" => "CSS makes your website beautiful."
-  ]
-];
-?>
+
 
 <!DOCTYPE html>
 <html>
@@ -22,17 +11,18 @@ $posts = [
 <header>
   <h1>My Mini Blog</h1>
 </header>
+<button id="loginBtn">Login</button>
+<button>Signup</button>
+<button>Back</button>
 
-<main>
-  <?php foreach ($posts as $post): ?>
-    <article>
-      <h2><?= $post["title"]; ?></h2>
-      <p><?= $post["content"]; ?></p>
-      <button class="toggle">Read more</button>
-    </article>
-  <?php endforeach; ?>
-</main>
+<div id="loginModal" style="display:none">
+  <input id="username" placeholder="Username">
+  <input id="password" type="password" placeholder="Password">
+  <button id="submitLogin">Submit</button>
+  <p id="errorMsg"></p>
+</div>
 
+<script src="script.js"></script>
 <script src="script.js"></script>
 </body>
 </html>
