645b2aa22d84279500ebe54c3efb1f19d008b0fa
hondaaja456 <honda456yuni77@gmail.com>
2026-01-26 21:36:02 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,9 @@
 // login.php
+
+<?php
+session_start();
+
+
 function login($username, $password) {
     $users = [
         "admin" => "1234",
@@ -23,11 +28,64 @@ if (login($username, $password)) {
 
 
 
-
+//confirmation only!
 //goes to dashboard php or index php, checkout php, profile php because needed to confirmed whetner its login or not for checking
 if (!isset($_SESSION['loggedin'])) {
     header("Location: index.php");
     exit;
 }
+
+
+
+
+
+
+
+
+
+//this code below if you dont js to redirect the page if log in was success
+
+// <?php
+// session_start();
+
+// function login($username, $password) {
+//     $users = [
+//         "admin" => "1234",
+//         "user" => "abcd"
+//     ];
+
+//     if (isset($users[$username]) && $users[$username] === $password) {
+//         $_SESSION['loggedin'] = true;
+//         $_SESSION['username'] = $username;
+//         return true;
+//     }
+
+//     return false;
+// }
+
+
+
+// $username = $_POST['username'] ?? ''; 
+// $password = $_POST['password'] ?? '';
+
+
+
+
+// if (login($username, $password)) {
+//     // Jump to dashboard echo "You are already logged in"
+//     header("Location: dashboard.php");
+//     exit;
+// } else {
+//     // POP-UP LOGIC:
+//     // We stop the PHP redirect and use JS to show the alert
+//     echo "<script>
+//         alert('Your login info is incorrect. Please try again.');
+//         window.location.href = 'index.php';
+//     </script>";
+//     exit;
+// }
+
+
+
 
 
