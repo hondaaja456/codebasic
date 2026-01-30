f353806a8995c9672da4f18f3eeab5124b031a4c
hondaaja456 <honda456yuni77@gmail.com>
2026-01-30 22:57:42 +0800

    Update signup.php

--- signup.php
+++ signup.php
@@ -7,3 +7,39 @@ $stmt = $pdo->prepare(
   "INSERT INTO users (email, password) VALUES (?, ?)"
 );
 $stmt->execute([$email, $hash]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+//this with email already exist else
+// <?php
+// require 'db.php';
+
+// $email = $_POST['email'] ?? '';
+// $password = $_POST['password'] ?? '';
+
+// if (!$email || !$password) {
+//     echo "Missing fields";
+//     exit;
+// }
+
+// $hash = password_hash($password, PASSWORD_DEFAULT);
+
+// $stmt = $pdo->prepare(
+//   "INSERT INTO users (email, password) VALUES (?, ?)"
+// );
+
+// try {
+//     $stmt->execute([$email, $hash]);
+//     echo "Signup successful. You can login now.";
+// } catch (PDOException $e) {
+//     echo "Email already exists";
